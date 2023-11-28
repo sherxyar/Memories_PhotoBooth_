97e9c86bad7c8cee6a8625b318a13c5948c38e2c
Mohammad Sheryar <shaheryartahir5@gmail.com>
2023-11-27 22:20:46 -0600

    Function working now

---
+++ js/eventHandlers.js
@@ -0,0 +1,1 @@
+document.getElementById("myDropdown").classList.toggle("show");
---
+++ js/functions.js
@@ -0,0 +1,20 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function quoteFunction() {
+    console.log("quoteFunction test");
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.quoteBtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
