83e5d677cbe9bafa0ae25fe9db467ebfad3a549a
Mohammad Sheryar <66784687+sherxyar@users.noreply.github.com>
2023-12-28 16:24:30 -0600

    slideshow working

--- js/functions.js
+++ js/functions.js
@@ -18,3 +18,49 @@ function quoteFunction() {
       }
     }
   }
+
+  //Slidshow function
+
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+  }
+  
+// Automatic slideshow
+let slideInterval;
+function startSlideshow() {
+  slideInterval = setInterval(function() {
+    plusSlides(1); // Move to the next slide
+  }, 2000); // Set the interval to 1 second (1000 milliseconds)
+}
+
+// Stop the slideshow if the user interacts with the slideshow manually
+function stopSlideshow() {
+  clearInterval(slideInterval);
+}
+
+
